24ffd7a0166675bbe366cb248ba78ec42737467f
Jaspher Jan Pasol <jpasol@sbitc.ph>
2018-10-27 09:23:09 +0800

    Saved

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -24,18 +24,45 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.TabPage1 = New System.Windows.Forms.TabPage()
-        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.MaskedTextBox6 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox5 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox4 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox3 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox2 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
         Me.TabPage4 = New System.Windows.Forms.TabPage()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
         Me.TabPage5 = New System.Windows.Forms.TabPage()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
         Me.TabPage6 = New System.Windows.Forms.TabPage()
+        Me.Button16 = New System.Windows.Forms.Button()
         Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage3.SuspendLayout()
+        Me.TabPage4.SuspendLayout()
+        Me.TabPage5.SuspendLayout()
+        Me.TabPage6.SuspendLayout()
         Me.SuspendLayout()
         '
         'TabControl1
         '
         Me.TabControl1.Controls.Add(Me.TabPage1)
-        Me.TabControl1.Controls.Add(Me.TabPage2)
         Me.TabControl1.Controls.Add(Me.TabPage3)
         Me.TabControl1.Controls.Add(Me.TabPage4)
         Me.TabControl1.Controls.Add(Me.TabPage5)
@@ -49,26 +76,147 @@ Partial Class Form1
         '
         'TabPage1
         '
+        Me.TabPage1.Controls.Add(Me.MaskedTextBox6)
+        Me.TabPage1.Controls.Add(Me.MaskedTextBox5)
+        Me.TabPage1.Controls.Add(Me.MaskedTextBox4)
+        Me.TabPage1.Controls.Add(Me.MaskedTextBox3)
+        Me.TabPage1.Controls.Add(Me.MaskedTextBox2)
+        Me.TabPage1.Controls.Add(Me.MaskedTextBox1)
+        Me.TabPage1.Controls.Add(Me.Label6)
+        Me.TabPage1.Controls.Add(Me.Label5)
+        Me.TabPage1.Controls.Add(Me.Label4)
+        Me.TabPage1.Controls.Add(Me.Label3)
+        Me.TabPage1.Controls.Add(Me.Label2)
+        Me.TabPage1.Controls.Add(Me.Label1)
+        Me.TabPage1.Controls.Add(Me.Button3)
+        Me.TabPage1.Controls.Add(Me.Button1)
         Me.TabPage1.Location = New System.Drawing.Point(4, 29)
         Me.TabPage1.Name = "TabPage1"
         Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
         Me.TabPage1.Size = New System.Drawing.Size(1318, 672)
         Me.TabPage1.TabIndex = 0
-        Me.TabPage1.Text = "Particulars: Details"
+        Me.TabPage1.Text = "Vessel Particulars"
         Me.TabPage1.UseVisualStyleBackColor = True
         '
-        'TabPage2
+        'MaskedTextBox6
         '
-        Me.TabPage2.Location = New System.Drawing.Point(4, 29)
-        Me.TabPage2.Name = "TabPage2"
-        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(1318, 672)
-        Me.TabPage2.TabIndex = 1
-        Me.TabPage2.Text = "Particulars: Details"
-        Me.TabPage2.UseVisualStyleBackColor = True
+        Me.MaskedTextBox6.Location = New System.Drawing.Point(271, 365)
+        Me.MaskedTextBox6.Name = "MaskedTextBox6"
+        Me.MaskedTextBox6.Size = New System.Drawing.Size(272, 27)
+        Me.MaskedTextBox6.TabIndex = 14
+        '
+        'MaskedTextBox5
+        '
+        Me.MaskedTextBox5.Location = New System.Drawing.Point(269, 307)
+        Me.MaskedTextBox5.Name = "MaskedTextBox5"
+        Me.MaskedTextBox5.Size = New System.Drawing.Size(272, 27)
+        Me.MaskedTextBox5.TabIndex = 13
+        '
+        'MaskedTextBox4
+        '
+        Me.MaskedTextBox4.Location = New System.Drawing.Point(256, 249)
+        Me.MaskedTextBox4.Name = "MaskedTextBox4"
+        Me.MaskedTextBox4.Size = New System.Drawing.Size(272, 27)
+        Me.MaskedTextBox4.TabIndex = 12
+        '
+        'MaskedTextBox3
+        '
+        Me.MaskedTextBox3.Location = New System.Drawing.Point(255, 191)
+        Me.MaskedTextBox3.Name = "MaskedTextBox3"
+        Me.MaskedTextBox3.Size = New System.Drawing.Size(194, 27)
+        Me.MaskedTextBox3.TabIndex = 11
+        '
+        'MaskedTextBox2
+        '
+        Me.MaskedTextBox2.Location = New System.Drawing.Point(250, 133)
+        Me.MaskedTextBox2.Name = "MaskedTextBox2"
+        Me.MaskedTextBox2.Size = New System.Drawing.Size(194, 27)
+        Me.MaskedTextBox2.TabIndex = 10
+        '
+        'MaskedTextBox1
+        '
+        Me.MaskedTextBox1.Location = New System.Drawing.Point(227, 75)
+        Me.MaskedTextBox1.Name = "MaskedTextBox1"
+        Me.MaskedTextBox1.Size = New System.Drawing.Size(535, 27)
+        Me.MaskedTextBox1.TabIndex = 9
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(105, 368)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(160, 22)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "Overtime Required"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(105, 310)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(158, 22)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "S/L Gang Request"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(105, 252)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(145, 22)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Pier Berth (NCT)"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(105, 194)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(144, 22)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Registry Number"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(105, 136)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(139, 22)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Voyage Number"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(105, 78)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(116, 22)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Vessel Name"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(1100, 569)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(164, 43)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Next (F11)"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(62, 569)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(164, 43)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Exit (F3)"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'TabPage3
         '
+        Me.TabPage3.Controls.Add(Me.Button7)
+        Me.TabPage3.Controls.Add(Me.Button8)
+        Me.TabPage3.Controls.Add(Me.Button9)
         Me.TabPage3.Location = New System.Drawing.Point(4, 29)
         Me.TabPage3.Name = "TabPage3"
         Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
@@ -77,8 +225,38 @@ Partial Class Form1
         Me.TabPage3.Text = "Throughput"
         Me.TabPage3.UseVisualStyleBackColor = True
         '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(1100, 569)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(164, 43)
+        Me.Button7.TabIndex = 5
+        Me.Button7.Text = "Next (F11)"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(930, 569)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(164, 43)
+        Me.Button8.TabIndex = 4
+        Me.Button8.Text = "Prev (F10)"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(62, 569)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(164, 43)
+        Me.Button9.TabIndex = 3
+        Me.Button9.Text = "Exit (F3)"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
         'TabPage4
         '
+        Me.TabPage4.Controls.Add(Me.Button10)
+        Me.TabPage4.Controls.Add(Me.Button11)
+        Me.TabPage4.Controls.Add(Me.Button12)
         Me.TabPage4.Location = New System.Drawing.Point(4, 29)
         Me.TabPage4.Name = "TabPage4"
         Me.TabPage4.Padding = New System.Windows.Forms.Padding(3)
@@ -87,8 +265,38 @@ Partial Class Form1
         Me.TabPage4.Text = "Specials"
         Me.TabPage4.UseVisualStyleBackColor = True
         '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(1100, 569)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(164, 43)
+        Me.Button10.TabIndex = 5
+        Me.Button10.Text = "Next (F11)"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(930, 569)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(164, 43)
+        Me.Button11.TabIndex = 4
+        Me.Button11.Text = "Prev (F10)"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(62, 569)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(164, 43)
+        Me.Button12.TabIndex = 3
+        Me.Button12.Text = "Exit (F3)"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
         'TabPage5
         '
+        Me.TabPage5.Controls.Add(Me.Button13)
+        Me.TabPage5.Controls.Add(Me.Button14)
+        Me.TabPage5.Controls.Add(Me.Button15)
         Me.TabPage5.Location = New System.Drawing.Point(4, 29)
         Me.TabPage5.Name = "TabPage5"
         Me.TabPage5.Padding = New System.Windows.Forms.Padding(3)
@@ -97,14 +305,51 @@ Partial Class Form1
         Me.TabPage5.Text = "Footers"
         Me.TabPage5.UseVisualStyleBackColor = True
         '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(1100, 569)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(164, 43)
+        Me.Button13.TabIndex = 5
+        Me.Button13.Text = "Next (F11)"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Location = New System.Drawing.Point(930, 569)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(164, 43)
+        Me.Button14.TabIndex = 4
+        Me.Button14.Text = "Prev (F10)"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(62, 569)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(164, 43)
+        Me.Button15.TabIndex = 3
+        Me.Button15.Text = "Exit (F3)"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
         'TabPage6
         '
+        Me.TabPage6.Controls.Add(Me.Button16)
         Me.TabPage6.Location = New System.Drawing.Point(4, 29)
         Me.TabPage6.Name = "TabPage6"
         Me.TabPage6.Size = New System.Drawing.Size(1318, 672)
         Me.TabPage6.TabIndex = 5
         Me.TabPage6.Text = "Preview"
         Me.TabPage6.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        Me.Button16.Location = New System.Drawing.Point(1100, 569)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(164, 43)
+        Me.Button16.TabIndex = 5
+        Me.Button16.Text = "Save | Print"
+        Me.Button16.UseVisualStyleBackColor = True
         '
         'Form1
         '
@@ -118,15 +363,44 @@ Partial Class Form1
         Me.Text = "Form1"
         Me.TopMost = True
         Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.TabPage3.ResumeLayout(False)
+        Me.TabPage4.ResumeLayout(False)
+        Me.TabPage5.ResumeLayout(False)
+        Me.TabPage6.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents TabControl1 As TabControl
     Friend WithEvents TabPage1 As TabPage
-    Friend WithEvents TabPage2 As TabPage
     Friend WithEvents TabPage3 As TabPage
     Friend WithEvents TabPage4 As TabPage
     Friend WithEvents TabPage5 As TabPage
     Friend WithEvents TabPage6 As TabPage
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents MaskedTextBox6 As MaskedTextBox
+    Friend WithEvents MaskedTextBox5 As MaskedTextBox
+    Friend WithEvents MaskedTextBox4 As MaskedTextBox
+    Friend WithEvents MaskedTextBox3 As MaskedTextBox
+    Friend WithEvents MaskedTextBox2 As MaskedTextBox
+    Friend WithEvents MaskedTextBox1 As MaskedTextBox
 End Class
